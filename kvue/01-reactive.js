527c9a526d6d502d3e2c11a76aa90f49225ce4df
kolinkuang <kolinkuang@github.com>
2021-02-12 21:04:34 +0800

    Reactive (2)

--- kvue/01-reactive.js
+++ kvue/01-reactive.js
@@ -1,6 +1,10 @@
 // 属性拦截： defineProperty()
 // Vue.util.
 function defineReactive(obj, key, val) {
+
+    // 递归判断
+    observe(val)
+
     // 属性拦截
     // 利用了闭包！！！
     // 1. 局部作用域
@@ -14,6 +18,8 @@ function defineReactive(obj, key, val) {
         set(newVal) {
             console.log('set', key)
             if (newVal !== val) {
+                // 新设置的值有可能是对象
+                observe(newVal)
                 val = newVal
             }
             // 很多人会疑问，val 明明是形参，为什么给他赋值就能够达到数值改变的效果呢？形参不是出了这个函数就没用了么？
@@ -26,8 +32,49 @@ function defineReactive(obj, key, val) {
 
 }
 
-const obj = {}
-defineReactive(obj, 'foo', 'fooo')
-defineReactive(obj, 'bar', 'barr')
-obj.foo
-obj.foo = 'foooooooooo'
+// 遍历需要响应式处理的对象
+function observe(obj) {
+    if (typeof obj !== 'object') {
+        return obj
+    }
+
+    // 遍历
+    Object.keys(obj).forEach(key => {
+        defineReactive(obj, key, obj[key])
+    })
+}
+
+// 如果用户有动态属性需要添加，需要使用 set
+function set(obj, key, val) {
+    defineReactive(obj, key, val)
+}
+
+const obj = {
+    foo: 'foo',
+    bar: 'bar',
+    a: {
+        n: 1
+    }
+}
+// defineReactive(obj, 'foo', 'fooo')
+// defineReactive(obj, 'bar', 'barr')
+observe(obj)
+
+// obj.foo
+// obj.foo = 'foooooooooo'
+// obj.a.n = 10
+// obj.a = {n: 10}
+// console.log('==============')
+// obj.a.n
+
+// obj.bla = 'blabla'
+
+// set(obj, 'bla', 'blabla')
+// obj.bla
+
+// Array
+// arr[0] = 'aaa'
+// arr[2]
+// 数组拦截需要覆盖 7 个变更方法：
+// push/pop/shift/unshift/...
+
